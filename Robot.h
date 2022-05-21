afafbbb35a05413ef2dfe58a24d2dc33f38a8e6c
AndyKong2020 <andy.kong@nuaa.edu.cn>
2022-05-21 23:05:57 +0800

    [refactor] class Robot

--- Robot.h
+++ Robot.h
@@ -8,9 +8,49 @@
 #ifndef CODE_ROBOT_H
 #define CODE_ROBOT_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 class Robot {
+public:
 
+    //攻击
+    virtual void Fight() = 0;
+
+    //发射
+    virtual void Hit() = 0;
+
+    //所属队伍
+    enum Team {kred, kblue};
+
+    //编号
+    unsigned short index_;
+
+    //血量上限
+    int total_health_;
+
+    //扣除血量
+    int health_minus;
+
+    //剩余血量
+    int health_point;
+
+    //武器口径
+    enum Caliber {kseventeen_mm, kfortytwo_mm};
+
+    //热量上限
+    int ceiling_heat;
+
+    //增加热量
+    int heat_add;
+
+    //目前热量
+    int heat_point;
+
+    //伤害值
+    int be_hit_;
 };
 
 
