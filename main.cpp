c2e70b55c44122175a38060f971920357de0d4e1
AndyKong2020 <andy.kong@nuaa.edu.cn>
2022-05-22 22:07:10 +0800

    [add]增加了main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 //作者:明月清风我
 
 #include <iostream>
+#include <string>
+#include "JudgeSystem.h"
+
+
 using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    JudgeSystem Js;
+    char order;
+    while (1)
+    {
+        cin >> order;
+        switch (order)
+        {
+            case 'A':
+                Js.Born();
+                break;
+            case 'F':
+                Js.Fight();
+                break;
+            case 'H':
+                Js.Hit();
+                break;
+            case 'E':
+                Js.ShowInfo();
+                break;
+
+
+        }
+    }
+
     return 0;
 }
